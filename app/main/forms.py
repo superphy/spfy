52de5f03bc1c12a3cbc3d5557e8898326ab190ce
Kevin Le <kevin.kent.le@gmail.com>
2017-02-13 10:03:22 -0700

    p3 vs p2

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,5 +7,4 @@ class TaskForm(Form):
     task = SelectField('Task')
 
     def __init__(self, *args, **kwargs):
-        super(Taskform, self).__init__(TaskForm, *args, **kwargs)
         self.task.choices = [(task, task) for task in current_app.config['TASKS']]
