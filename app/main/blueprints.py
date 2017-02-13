761b0ebeb3ef41e4356203e90ab8b71be70e041f
Kevin Le <kevin.kent.le@gmail.com>
2017-02-13 09:59:06 -0700

    test

--- app/main/blueprints.py
+++ app/main/blueprints.py
@@ -2,7 +2,7 @@ import redis
 from flask import Blueprint, render_template, request, jsonify, current_app, g, url_for
 from rq import push_connection, pop_connection, Queue
 
-from forms import TaskForm
+from .forms import TaskForm
 from .. import tasks
 
 bp = Blueprint('main', __name__)
