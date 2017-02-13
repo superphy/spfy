e725bea0c5ddb6870e8b7c5e8866a8c94775f077
Kevin Le <kevin.kent.le@gmail.com>
2017-02-13 10:16:11 -0700

    updated redis-url

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 import os
 
 SECRET_KEY = 'can-you-keep-a-secret'
-REDIS_URL = 'redis://redis:6379/0'
+REDIS_URL = '0.0.0.0/6379'
 QUEUES = ['high,medium,low,default']
 BOOTSTRAP_SERVE_LOCAL = True
 TASKS = ['Short task', 'Long task', 'Task raises error']
