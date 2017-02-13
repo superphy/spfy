43ed18c233242ba35118be509de1d4c1a4fa5d3b
Kevin Le <kevin.kent.le@gmail.com>
2017-02-13 15:12:27 -0700

    test

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ import redis
 from flask import Blueprint, render_template, request, jsonify, current_app, g, url_for
 from rq import push_connection, pop_connection, Queue
 
-from .forms import TaskForm, UploadForm
+from .forms import UploadForm
 from .. import tasks
 
 
