565b0b482993c9efc7f671ea2453b6f68ed26c4b
Kevin Le <kevin.kent.le@gmail.com>
2017-02-14 15:12:14 -0700

    deleted img previews

--- app/static/js/upload.js
+++ app/static/js/upload.js
@@ -30,47 +30,12 @@ $(function() {
             }
         },
 
-        uploadStarted: function(i, file, len) {
-            createImage(file);
-        },
-
-        progressUpdated: function(i, file, progress) {
-            $.data(file).find('.progress').width(progress);
-        }
 
     });
 
-    var template = '<div class="preview">' +
-    '<span class="imageHolder">' +
-    '<img />' +
-    '<span class="uploaded"></span>' +
-    '</span>' +
-    '<div class="progressHolder">' +
-    '<div class="progress"></div>' +
-    '</div>' +
-    '</div>';
 
-    function createImage(file) {
 
-        var preview = $(template),
-            image = $('img', preview);
 
-        var reader = new FileReader();
-
-        image.width = 100;
-        image.height = 100;
-
-        reader.onload = function(e) {
-            image.attr('src', e.target.result);
-        };
-
-        reader.readAsDataURL(file);
-
-        message.hide();
-        preview.appendTo(dropbox);
-
-        $.data(file, preview);
-    }
 
     function showMessage(msg) {
         message.html(msg);
