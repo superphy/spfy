2e687d7880fc28de524cf2213727bb14eb81463f
Kevin Le <kevin.kent.le@gmail.com>
2017-02-14 16:24:20 -0700

    lets trya  low job which takes longer

--- app/spfy.py
+++ app/spfy.py
@@ -43,15 +43,15 @@ def blob_savvy(args_dict):
         for f in os.listdir(args_dict['i']):
             single_dict = dict(args_dict.items() + {'uriIsolate': args_dict['uris'][f][
                                'uriIsolate'], 'uriGenome': args_dict['uris'][f]['uriGenome'], 'i': args_dict['i'] + f, 'uris': None}.items())
-            job = high.enqueue(savvy, dict(single_dict.items() +
+            high.enqueue(savvy, dict(single_dict.items() +
                                      {'disable_amr': True}.items()),result_ttl=-1)
-            low.enqueue(savvy, dict(single_dict.items() +
+            job = low.enqueue(savvy, dict(single_dict.items() +
                                     {'disable_vf': True, 'disable_serotype': True}.items()),result_ttl=-1)
     else:
         # run the much faster vf and serotyping separately of amr
-        job = high.enqueue(savvy, dict(args_dict.items() +
+        high.enqueue(savvy, dict(args_dict.items() +
                                  {'disable_amr': True}.items()),result_ttl=-1)
-        low.enqueue(savvy, dict(args_dict.items() +
+        job = low.enqueue(savvy, dict(args_dict.items() +
                                 {'disable_vf': True, 'disable_serotype': True}.items()),result_ttl=-1)
 
     return job
