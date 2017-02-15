80582b7a38c96e04e567483cdf4bc2e6718e3e7d
Kevin Le <kevin.kent.le@gmail.com>
2017-02-15 14:51:03 -0700

    try this before changing names

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -118,4 +118,19 @@
      };
   }]);
 
+  .directive('file', function () {
+    return {
+        scope: {
+            file: '='
+        },
+        link: function (scope, el, attrs) {
+            el.bind('change', function (event) {
+                var file = event.target.files[0];
+                scope.file = file ? file : undefined;
+                scope.$apply();
+            });
+        }
+    };
+});
+
 }());
