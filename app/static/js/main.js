3ac2647d1626aa86d4efd72025eaa25efd6d5618
Kevin Le <kevin.kent.le@gmail.com>
2017-02-15 14:53:20 -0700

    dot errors?

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -118,19 +118,6 @@
      };
   }]);
 
-  .directive('file', function () {
-    return {
-        scope: {
-            file: '='
-        },
-        link: function (scope, el, attrs) {
-            el.bind('change', function (event) {
-                var file = event.target.files[0];
-                scope.file = file ? file : undefined;
-                scope.$apply();
-            });
-        }
-    };
-});
+
 
 }());
