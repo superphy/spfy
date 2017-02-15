e0a7db177fd37542786114c0f67b4d828661fbf2
Kevin Le <kevin.kent.le@gmail.com>
2017-02-15 14:15:09 -0700

    directives and form data

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,121 +1,129 @@
-(function () {
+(function() {
 
-  'use strict';
+    'use strict';
 
-  angular.module('WordcountApp', [])
+    angular.module('WordcountApp', []).controller('WordcountController', [
+        '$scope',
+        '$log',
+        '$http',
+        '$timeout',
+        function($scope, $log, $http, $timeout) {
 
-  .controller('WordcountController', ['$scope', '$log', '$http', '$timeout',
-    function($scope, $log, $http, $timeout) {
+            $scope.submitButtonText = 'Submit';
+            $scope.loading = false;
+            $scope.urlerror = false;
 
-    $scope.submitButtonText = 'Submit';
-    $scope.loading = false;
-    $scope.urlerror = false;
+            $scope.getResults = function() {
 
-    $scope.getResults = function() {
+                $log.log('test');
 
-      $log.log('test');
+                // get the URL from the input
+                var userInput = $scope.url;
 
-      // get the URL from the input
-      var userInput = $scope.url;
+                // fire the API request
+                $http({
+                    method: 'POST',
+                    url: '/upload',
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    },
+                    data: {
+                        upload: $scope.file
+                    },
+                    transformRequest: function(data, headersGetter) {
+                        var formData = new FormData();
+                        angular.forEach(data, function(value, key) {
+                            formData.append(key, value);
+                        });
 
-      // fire the API request
-      $http({
-            method: 'POST',
-            url: '/upload',
-            headers: {
-                'Content-Type': 'multipart/form-data'
-            },
-            data: {
-                upload: $scope.file
-            },
-            transformRequest: function (data, headersGetter) {
-                var formData = new FormData();
-                angular.forEach(data, function (value, key) {
-                    formData.append(key, value);
+                        var headers = headersGetter();
+                        delete headers['Content-Type'];
+
+                        return formData;
+                    }
+                }).success(function(results) {
+                    $log.log(results);
+                    getWordCount(results);
+                    $scope.wordcounts = null;
+                    $scope.loading = true;
+                    $scope.submitButtonText = 'Loading...';
+                    $scope.urlerror = false;
+                }).error(function(error) {
+                    $log.log(error);
                 });
 
-                var headers = headersGetter();
-                delete headers['Content-Type'];
+            };
 
-                return formData;
+            function getWordCount(jobID) {
+
+                var timeout = '';
+
+                var poller = function() {
+                    // fire another request
+                    $http.get('/results/' + jobID).success(function(data, status, headers, config) {
+                        if (status === 202) {
+                            $log.log(data, status);
+                        } else if (status === 200) {
+                            $log.log(data);
+                            $scope.loading = false;
+                            $scope.submitButtonText = "Submit";
+                            $scope.wordcounts = data;
+                            $timeout.cancel(timeout);
+                            return false;
+                        }
+                        // continue to call the poller() function every 2 seconds
+                        // until the timeout is cancelled
+                        timeout = $timeout(poller, 2000);
+                    }).error(function(error) {
+                        $log.log(error);
+                        $scope.loading = false;
+                        $scope.submitButtonText = "Submit";
+                        $scope.urlerror = true;
+                    });
+                };
+
+                poller();
+
             }
-        }).
-        success(function(results) {
-          $log.log(results);
-          getWordCount(results);
-          $scope.wordcounts = null;
-          $scope.loading = true;
-          $scope.submitButtonText = 'Loading...';
-          $scope.urlerror = false;
-        }).
-        error(function(error) {
-          $log.log(error);
-        });
 
-    };
-
-    function getWordCount(jobID) {
-
-      var timeout = '';
-
-      var poller = function() {
-        // fire another request
-        $http.get('/results/'+jobID).
-          success(function(data, status, headers, config) {
-            if(status === 202) {
-              $log.log(data, status);
-            } else if (status === 200){
-              $log.log(data);
-              $scope.loading = false;
-              $scope.submitButtonText = "Submit";
-              $scope.wordcounts = data;
-              $timeout.cancel(timeout);
-              return false;
+        }
+    ]).directive('wordCountChart', [
+        '$parse',
+        function($parse) {
+            return {
+                restrict: 'E',
+                replace: true,
+                template: '<div id="chart"></div>',
+                link: function(scope) {
+                    scope.$watch('wordcounts', function() {
+                        d3.select('#chart').selectAll('*').remove();
+                        var data = scope.wordcounts;
+                        for (var word in data) {
+                            d3.select('#chart').append('div').selectAll('div').data(word[0]).enter().append('div').style('width', function() {
+                                return (data[word] * 20) + 'px';
+                            }).text(function(d) {
+                                return word;
+                            });
+                        }
+                    }, true);
+                }
+            };
+        }
+    ]);.directive('file', function() {
+        return {
+            scope: {
+                file: '='
+            },
+            link: function(scope, el, attrs) {
+                el.bind('change', function(event) {
+                    var file = event.target.files[0];
+                    scope.file = file
+                        ? file
+                        : undefined;
+                    scope.$apply();
+                });
             }
-            // continue to call the poller() function every 2 seconds
-            // until the timeout is cancelled
-            timeout = $timeout(poller, 2000);
-          }).
-          error(function(error) {
-            $log.log(error);
-            $scope.loading = false;
-            $scope.submitButtonText = "Submit";
-            $scope.urlerror = true;
-          });
-      };
-
-      poller();
-
-    }
-
-  }])
-
-  .directive('wordCountChart', ['$parse', function ($parse) {
-    return {
-      restrict: 'E',
-      replace: true,
-      template: '<div id="chart"></div>',
-      link: function (scope) {
-        scope.$watch('wordcounts', function() {
-          d3.select('#chart').selectAll('*').remove();
-          var data = scope.wordcounts;
-          for (var word in data) {
-            d3.select('#chart')
-              .append('div')
-              .selectAll('div')
-              .data(word[0])
-              .enter()
-              .append('div')
-              .style('width', function() {
-                return (data[word] * 20) + 'px';
-              })
-              .text(function(d){
-                return word;
-              });
-          }
-        }, true);
-      }
-     };
-  }]);
+        };
+    });
 
 }());
