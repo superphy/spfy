87aed9a91c2e892e98726e4327e2b8991f575ea3
Kevin Le <kevin.kent.le@gmail.com>
2017-02-16 11:52:49 -0700

    not working, debugging

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -67,7 +67,7 @@ var app =  angular.module('WordcountApp', [])
           });
       };
 
-      angular.forEach(results, poller(value, jobID));
+      angular.forEach(results, poller(Object.keys(results)));
 
     }
 
