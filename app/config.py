075f49a04e8291c9975fe6ae44d77b3d42e6143b
Kevin Le <kevin.kent.le@gmail.com>
2017-02-16 11:31:38 -0700

    fixed bug in config

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 SECRET_KEY = 'is-that-daisy'
 REDIS_URL = '0.0.0.0/6379'
-QUEUES = ['high,medium,low,default']
+QUEUES = ['high','medium','low','default']
 BOOTSTRAP_SERVE_LOCAL = True
 MAX_TIME_TO_WAIT = 10
 
