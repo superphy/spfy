2283524e2762bb2d7c4ede8e3485709bea575a80
Kevin Le <kevin.kent.le@gmail.com>
2017-02-20 11:29:02 -0700

    test

--- app/main/views.py
+++ app/main/views.py
@@ -60,7 +60,8 @@ def upload():
                 tar.close()
                 #set filename to dir for spfy call
                 # tar.extractall will create a another folder at targer directory under filename
-                filename = d + '/' + os.path.basename(fname)
+                tname, textension = os.path.splitext(fname)
+                filename = d + '/' + tname
                 print 'filename in if is ' + filename
             else:
                 filename = os.path.join(current_app.config['UPLOAD_FOLDER'], now + file.filename.rsplit('.', 1)[1])
