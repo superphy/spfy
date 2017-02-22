522cdb88fabfe4a9921410134dc98f860619165c
Kevin Le <kevin.kent.le@gmail.com>
2017-02-22 12:36:50 -0700

    test

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -94,3 +94,32 @@ app.directive('fileModel', [
         };
     }
 ]);
+
+app.directive('exportToCsv',function(){
+  	return {
+    	restrict: 'A',
+    	link: function (scope, element, attrs) {
+    		var el = element[0];
+	        element.bind('click', function(e){
+	        	var table = e.target.nextElementSibling;
+	        	var csvString = '';
+	        	for(var i=0; i<table.rows.length;i++){
+	        		var rowData = table.rows[i].cells;
+	        		for(var j=0; j<rowData.length;j++){
+	        			csvString = csvString + rowData[j].innerHTML + ",";
+	        		}
+	        		csvString = csvString.substring(0,csvString.length - 1);
+	        		csvString = csvString + "\n";
+			    }
+	         	csvString = csvString.substring(0, csvString.length - 1);
+	         	var a = $('<a/>', {
+		            style:'display:none',
+		            href:'data:application/octet-stream;base64,'+btoa(csvString),
+		            download:'results.csv'
+		        }).appendTo('body')
+		        a[0].click()
+		        a.remove();
+	        });
+    	}
+  	}
+	});
