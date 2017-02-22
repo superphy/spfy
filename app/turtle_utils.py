0749155fe6cc071dd5d722ddf519415d1aa308a9
Kevin Le <kevin.kent.le@gmail.com>
2017-02-22 16:12:13 -0700

    bug fix in progress

--- app/turtle_utils.py
+++ app/turtle_utils.py
@@ -13,6 +13,7 @@ def slugify(value):
     and converts spaces to hyphens.
     """
     import unicodedata
+    value = unicode(value)
     value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
     value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
     value = unicode(re.sub('[-\s]+', '-', value))
