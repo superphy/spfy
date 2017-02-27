3174849f32aae5d1db1a8fc06a62ef3d7e2b74f0
Kevin Le <kevin.kent.le@gmail.com>
2017-02-27 13:41:40 -0700

    syntax

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -97,7 +97,7 @@ app.controller('SpfyController', [
                         $scope.urlerror = true;
                         $scope.jobfailed = true;
                         $log.log($scope.jobfailed);
-                        $scope.message = $scope.message + key;
+                        $scope.message = $scope.message + "Job failed. Key: " + key;
                     });
                 };
 
