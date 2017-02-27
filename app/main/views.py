1fe988b958aab77568ebb1425a0f8f5ce2354c52
Kevin Le <kevin.kent.le@gmail.com>
2017-02-27 11:58:24 -0700

    update

--- app/main/views.py
+++ app/main/views.py
@@ -33,7 +33,7 @@ def job_status(job_id):
     if job.is_finished:
         return jsonify(job.result)
     elif job.is_failed:
-        return jsonify({'message':job.exc_info.strip().split('\n')[-1]})
+        return jsonify({'message': job_id + ' has failed. Exect Info: ' + job.exc_info})
     else:
         return "Still pending", 202
 
