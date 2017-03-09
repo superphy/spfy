7dd2b8675ab4331f66ee8238a4522668b9eea530
Kevin Le <kevin.kent.le@gmail.com>
2017-03-09 16:43:37 -0700

    cleanup

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
 
+#for secret key, do a os.urandom(24).encode('hex')
 SECRET_KEY = 'is-that-daisy'
 REDIS_URL = '0.0.0.0/6379'
 QUEUES = ['high','medium','low','default']
@@ -9,5 +10,5 @@ MAX_TIME_TO_WAIT = 10
 UPLOAD_FOLDER = 'app/tmp'
 ALLOWED_EXTENSIONS = ['fna']
 RECAPTCHA_ENABLED = True
-RECAPTCHA_SITE_KEY = ""
-RECAPTCHA_SECRET_KEY = "6LeVYhgUAAAAAKbedEJoCcRaeFaxPh-2hZfzXfFP"
+RECAPTCHA_SITE_KEY = "6LeVYhgUAAAAAKbedEJoCcRaeFaxPh-2hZfzXfFP"
+RECAPTCHA_SECRET_KEY = "PUTYOSECRETKEYHERE"
