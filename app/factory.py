b1eb6f11e19f160f5140c26bb61f14dd836ea58c
Kevin Le <kevin.kent.le@gmail.com>
2017-03-15 16:55:08 -0600

    debugging ValueError: Attempted relative import in non-package

--- app/factory.py
+++ app/factory.py
@@ -4,8 +4,8 @@ this is the app factory
 
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from . import config
-from .spfyroutes.views import bp as spfy
+import config
+from spfyroutes.views import bp as spfy
 from flask_recaptcha import ReCaptcha
 
 
