19e0349683a809cdfd55df5cd6c0d7b48a6ac725
Kevin Le <kevin.kent.le@gmail.com>
2017-03-24 15:03:41 -0600

    dirs

--- tests/test_modules_qc.py
+++ tests/test_modules_qc.py
@@ -1,8 +1,11 @@
 import os
 from app.modules.qc.qc import qc
 
+ecoli_dir = os.path.abspath('ecoli')
+not_ecoli_dir = os.path.abspath('notEcoli')
+
 def test_modules_qc():
-    for f in os.listdir('ecoli'):
+    for f in ecoli_dir:
         assert qc(f) == True
-    for f in os.listdir('notEcoli'):
+    for f in not_ecoli_dir:
         assert qc(f) == False
