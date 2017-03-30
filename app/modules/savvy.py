2fc6c39130550c94da4b9a5275157d84075081d8
Kevin Le <kevin.kent.le@gmail.com>
2017-03-30 15:19:37 -0600

    begin testing through draft of savvy.py cli tool

--- app/modules/savvy.py
+++ app/modules/savvy.py
@@ -7,72 +7,41 @@
 # A:ntimicrobial Resistance
 # V:irulence Factors
 # -vy
+# essentially implements the same pipeline as blob_savvy_enqueue() in spfy.py, but without the RQ backing and with option graph generation of outputs
 
 import logging
 
-from app.modules.turtleGrapher.turtle_utils import generate_uri as gu
-from app.modules.turtleGrapher.turtle_grapher import generate_graph, generate_hash, generate_turtle_skeleton
-
-from app.modules.blazeUploader.upload_graph import upload_graph
-from app.modules.beautify.beautify import json_return
-
+from app.modules.qc.qc import qc
+from app.modules.blazeUploader.reserve_id import write_reserve_id
 from app.modules.ectyper.call_ectyper import call_ectyper
+from app.modules.amr.amr import amr
+from app.modules.amr.amr_to_dict import amr_to_dict
+from app.modules.beautify.beautify import beautify
+from app.modules.turtleGrapher.datastruct_savvy import datastruct_savvy
+from app.modules.turtleGrapher.turtle_grapher import turtle_grapher
 
-# for pairwise comparison of rows in panadas
+log = logging.getLogger(__name__)
 
 def savvy(args_dict):
     '''
-    Args:
-        args_dict(dict): i prefer working with args in a dictionary, rather than a namespace is all
 
-    Returns:
-        (rdflib.Graph): a graph object with the VF/AMR/Serotype added to it via ECTyper/RGI
     '''
-    from os import remove  # for batch cleanup
-    # starting #logging
-    '''
-    logging.basicConfig(
-        filename='outputs/' + __name__ +
-        args_dict['i'].split('/')[-1] + '.log',
-        level=logging.INFO
-    )
-    '''
+    log.info("Begin Savvy()")
+    log.debug("args_dict received was: " + str(args_dict))
 
-    print("Importing FASTA from: " + args_dict['i'])
-    #logging.info('importing from' + args_dict['i'])
+    query_file = args_dict['i']
+    log.info("query_file is: " + query_file)
 
-    #saving copy of filename, this is req due to hack used to handle ectyper
-    args_dict['filename'] = args_dict['i']
+    # (1) QC Step:
+    qc_pass = qc(query_file)
+    assert qc(query_file) == True
+    log.info("QC: " + str(qc_pass))
 
-    # setting up graph
-    graph = generate_graph()
+    # (2) SpfyID Step:
 
-    ectyper_result={}
-
-    #logging.info('generating barebones ttl from file')
-    graph = generate_turtle_skeleton(
-        graph, args_dict['i'], args_dict['uriIsolate'], args_dict['uriGenome'])
-    #logging.info('barebones ttl generated')
-
-    if not (args_dict['disable_serotype'] and args_dict['disable_vf'] and args_dict['disable_amr']):
-        #logging.info('calling ectyper')
-        ectyper_result = call_ectyper(graph, args_dict)
-        graph = ectyper_result['graph']
-        #logging.info('ectyper call completed')
-
-    # individual fasta logs are wiped on completion (or you'd have several
-    # thousand of these)
-    #remove('outputs/' + __name__ + args_dict['i'].split('/')[-1] + '.log')
-    print upload_graph(graph)
-
-    if not (args_dict['disable_serotype'] and args_dict['disable_vf'] and args_dict['disable_amr']):
-        return json_return(args_dict, ectyper_result['ectyper_dict'])
 
 if __name__ == "__main__":
     import argparse
-    import os  # for batch cleanup
-
-    from ConfigParser import SafeConfigParser
 
     # parsing cli-input
     parser = argparse.ArgumentParser()
@@ -96,50 +65,13 @@ if __name__ == "__main__":
         help="Disables use of RGI to get Antimicrobial Resistance Factors.  AMR genes are computed by default.",
         action="store_true"
     )
-    # note: by in large, we expect uri to be given as just the unique string
-    # value  (be it the hash or the integer) without any prefixes, the actual
-    # rdflib.URIRef object will be generated in this script
-    # this is mainly for batch computation
-    parser.add_argument(
-        "--uriGenome",
-        help="Allows the specification of the Genome URI separately. Expect just the hash (not an actual uri).",
-    )
-    # This is both for batch computation and for future extensions where there
-    # are multiple sequencings per isolate (Campy)
-    parser.add_argument(
-        "--uriIsolate",
-        help="Allows the specification of the Isolate URI separately. Expect just the integer (not the full :spfyID)",
-        type=int
-    )
+    parser.add_argument("--pi",
+                        type=int,
+                        help="Percentage of identity wanted to use against the database. From 0 to 100, default is 90%.",
+                        default=90, choices=range(0, 100))
     args = parser.parse_args()
     # we make a dictionary from the cli-inputs and add are uris to it
     # mainly used for when a func needs a lot of the args
     args_dict = vars(args)
 
-    # starting#logging
-    '''
-    logging.basicConfig(
-        filename='outputs/' + __name__ +
-        args_dict['i'].split('/')[-1] + '.log',
-        level=logging.INFO
-    )
-    '''
-
-    # check if a genome uri isn't set yet
-    if args_dict['uriIsolate'] is None:
-        # this is temporary, TODO: include a spqarql query to the db
-        uriIsolate = gu(':spfy' + str(hash(args_dict['i'].split('/')[-1])))
-    else:
-        uriIsolate = gu(':spfy' + args_dict['uriIsolate'])
-
-    # if the fasta_file hash was not precomputed (batch scripts should
-    # precompute it), we compute that now
-    if args_dict['uriGenome'] is None:
-        uriGenome = gu(':' + generate_hash(args_dict['i']))
-    else:
-        uriGenome = gu(':' + args_dict['uriGenome'])
-
-    args_dict['uriIsolate'] = uriIsolate
-    args_dict['uriGenome'] = uriGenome
-
-    print savvy(args_dict)
+    savvy(args_dict)
