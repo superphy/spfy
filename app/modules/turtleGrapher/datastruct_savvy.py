2bb848960c63c0b048001eea5edb0864af6f78ae
Kevin Le <kevin.kent.le@gmail.com>
2017-03-31 12:18:21 -0600

    fixed missing import in datastruct_savvy

--- app/modules/turtleGrapher/datastruct_savvy.py
+++ app/modules/turtleGrapher/datastruct_savvy.py
@@ -1,6 +1,6 @@
 import cPickle as pickle
 from rdflib import BNode, Literal, Graph
-from app.modules.turtleGrapher.turtle_utils import generate_uri as gu
+from app.modules.turtleGrapher.turtle_utils import generate_uri as gu, generate_hash
 from app.modules.turtleGrapher.turtle_grapher import generate_graph
 from app.modules.blazeUploader.upload_graph import upload_graph
 # working with Serotype, Antimicrobial Resistance, & Virulence Factor data
