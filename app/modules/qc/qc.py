afb93aa4921f1395f5d00fb3fdd5f875d95d9387
Kevin Le <kevin.kent.le@gmail.com>
2017-04-03 14:23:17 -0600

    new qc testing

--- app/modules/qc/qc.py
+++ app/modules/qc/qc.py
@@ -5,6 +5,9 @@ import argparse
 
 import pandas as pd
 
+# we want to use true division, not integer
+from __future__ import division
+
 def create_blast_db():
     '''
     Creates a reference database using https://raw.githubusercontent.com/superphy/version-1/master/Sequences/genome_content_panseq/putative_e_coli_specific.fasta
@@ -22,7 +25,7 @@ def create_blast_db():
     if ret_code == 0:
         return blast_db_path
     else:
-        raise
+        raise Exception("Could not create blast db")
 
 
 def run_blast(query_file, blast_db):
@@ -35,12 +38,12 @@ def run_blast(query_file, blast_db):
                                         "-query", query_file,
                                         "-db", blast_db,
                                         "-out", blast_output_file,
-                                        "-outfmt", "10",
+                                        "-outfmt", '10 " qseqid qlen sseqid length pident sstart send sframe "',
                                         "-word_size", "11"])
     if ret_code == 0:
         return blast_output_file
     else:
-        raise
+        raise Exception("Could not run blast")
 
 def parse_blast_records(blast_output_file):
     '''
@@ -50,11 +53,14 @@ def parse_blast_records(blast_output_file):
     returns a list of unique hits from the reference db
     '''
     blast_records = pd.read_csv(blast_output_file, header=None)
-    # col 2 is percent identity
-    blast_records_perfect = blast_records[blast_records.iloc[:,2]==100]
+    # col 4 is percent identity, as set by our blast call above
+    blast_records_pi_passed = blast_records[blast_records.iloc[:,4]>=90]
+
+    # pl check: col
+    blast_records_pi_pl_passed = blast_records_pi_passed[blast_records_pi_passed.iloc[:,3]/blast_records_pi_passed.iloc[:,1]>=90]
 
     # col 1 is the subject (where col 0 is the query)
-    unique_hits = blast_records_perfect.iloc[:,1].unique()
+    unique_hits = blast_records_pi_pl_passed.iloc[:,1].unique()
 
     return unique_hits
 
