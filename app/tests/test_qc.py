3d461b79079dc9f9d93fc6245e85536914feb8a7
Kevin Le <kevin.kent.le@gmail.com>
2017-04-04 11:31:23 -0600

    added qc test

---
+++ app/tests/test_qc.py
@@ -0,0 +1,22 @@
+import pytest
+import os
+
+from modules.qc.qc import qc
+from modules.blazeUploader.reserve_id import write_reserve_id
+from modules.ectyper.call_ectyper import call_ectyper
+from modules.amr.amr import amr
+from modules.amr.amr_to_dict import amr_to_dict
+from modules.beautify.beautify import beautify
+from modules.turtleGrapher.datastruct_savvy import datastruct_savvy
+from modules.turtleGrapher.turtle_grapher import turtle_grapher
+
+GENOMES_LIST_NOT_ECOLI = os.listdir('notEcoli')
+GENOMES_LIST_ECOLI = os.listdir('ecoli')
+
+def test_qc():
+    for ecoli_genome in GENOMES_LIST_ECOLI:
+        print ecoli_genome
+        assert qc(non_ecoli_genome) == True
+    for non_ecoli_genome in GENOMES_LIST_NOT_ECOLI:
+        print non_ecoli_genome
+        assert qc(non_ecoli_genome) == False
