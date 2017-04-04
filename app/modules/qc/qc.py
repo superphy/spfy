9f47f6ebefc28b8d363e561f35a3cbbe818d3758
Kevin Le <kevin.kent.le@gmail.com>
2017-04-04 10:06:54 -0600

    new draft

--- app/modules/qc/qc.py
+++ app/modules/qc/qc.py
@@ -58,7 +58,9 @@ def parse_blast_records(blast_output_file):
     # col 4 is percent identity, as set by our blast call above
     blast_records_pi_passed = blast_records[blast_records['pident']>=90]
     # pl check: col
-    blast_records_pi_pl_passed = blast_records_pi_passed[blast_records_pi_passed[blast_records_pi_passed['length']/blast_records_pi_passed['qlen'] * 100 >= 90]]
+    blast_records_pi_passed['pl'] = blast_records_pi_passed['length']/blast_records_pi_passed['qlen'] * 100
+    print blast_records_pi_passed
+    blast_records_pi_pl_passed = blast_records_pi_passed[blast_records_pi_passed['pl'] >= 90]
     print blast_records_pi_pl_passed
     # col 1 is the subject (where col 0 is the query)
     unique_hits = blast_records_pi_pl_passed['sseqid'].unique()
