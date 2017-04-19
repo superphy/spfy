2393b96fe2c1cd159ee06538c5bf130666d82693
Kevin Le <kevin.kent.le@gmail.com>
2017-04-19 14:10:01 -0600

    print log location

--- app/modules/blazeUploader/reserve_id.py
+++ app/modules/blazeUploader/reserve_id.py
@@ -134,4 +134,5 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", required=True)
     args = parser.parse_args()
+    print log
     print reserve_id(args.i)
