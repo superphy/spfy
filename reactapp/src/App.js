cd00d93d24f2e35566ed2eeec0bd68079786ec69
Kevin Le <kevin.kent.le@gmail.com>
2017-04-24 20:35:20 -0600

    START: reactmd nav

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import NavigationDrawer from 'react-md/lib/NavigationDrawers';
-import logo from './logo.svg';
-import './App.scss';
+import NavLink from './NavLink';
+
+import Home from './Home';
+import Page1 from './Page1';
+import Page2 from './Page2';
+import Page3 from './Page3';
+
+const navItems = [{
+  exact: true,
+  label: 'Home',
+  to: '/',
+  icon: 'home',
+}, {
+  label: 'Page 1',
+  to: '/page-1',
+  icon: 'bookmark',
+}, {
+  label: 'Page 2',
+  to: '/page-2',
+  icon: 'donut_large',
+}, {
+  label: 'Page 3',
+  to: '/page-3',
+  icon: 'flight_land',
+}];
 
 class App extends Component {
   render() {
     return (
-      <NavigationDrawer
-        drawerTitle="react-md with CRA"
-        toolbarTitle="Welcome to react-md"
-      >
-        <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to React</h2>
-          </div>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
-      </NavigationDrawer>
+      <Route
+        render={({ location }) => (
+          <NavigationDrawer
+            drawerTitle="react-md with CRA"
+            toolbarTitle="Welcome to react-md"
+            navItems={navItems.map(props => <NavLink {...props} key={props.to} />)}
+          >
+            <Switch key={location.key}>
+              <Route exact path="/" location={location} component={Home} />
+              <Route path="/page-1" location={location} component={Page1} />
+              <Route path="/page-2" location={location} component={Page2} />
+              <Route path="/page-3" location={location} component={Page3} />
+            </Switch>
+          </NavigationDrawer>
+        )}
+      />
     );
   }
 }
