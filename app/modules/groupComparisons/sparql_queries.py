e39d3be38bf9b0143a711eaa31cda588a3250731
Kevin Le <kevin.kent.le@gmail.com>
2017-04-26 08:53:21 -0600

    ADD: code to check sparql_query outputs

--- app/modules/groupComparisons/sparql_queries.py
+++ app/modules/groupComparisons/sparql_queries.py
@@ -12,6 +12,41 @@ log = logging.getLogger(__name__)
 #blazegraph_url = config.database['blazegraph_url']
 blazegraph_url = 'http://localhost:8080/bigdata/sparql'
 
+def get_attribute_values(objectTypeUri, attributeTypeUri):
+    '''
+    Given an objectype(ex. :spfyId) and an attribute type(ex. so:0001076, aka. O-Type).
+    Returns a list of all distinct attribute values associated with those two conditions.
+    '''
+    # SPARQL Query
+    sparql = SPARQLWrapper(blazegraph_url)
+    query = """
+    SELECT DISTINCT ?attribute WHERE {{
+        ?s a {objectTypeUri} ; {attributeTypeUri} ?attribute .
+    }}
+    """.format(objectTypeUri=objectTypeUri, attributeTypeUri=attributeTypeUri)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    return parse_results(results, 'attribute', attributeTypeUri)
+
+def get_attribute_types(objectTypeUri):
+    '''
+    Given a object type uri, returns a simple list of all distinct attribute types it has.
+    '''
+    # SPARQL Query
+    sparql = SPARQLWrapper(blazegraph_url)
+    query = """
+    SELECT DISTINCT ?attributetype WHERE {{
+        ?s a {objectTypeUri} ; ?attributetype ?attribute .
+    }}
+    """.format(objectTypeUri=objectTypeUri)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
+
+    return parse_results(results, 'attributetype', objectTypeUri)
+
 def get_instances(objectTypeUri):
     '''
     Gets all instances of a given object type.
@@ -19,7 +54,7 @@ def get_instances(objectTypeUri):
     Args:
         objecttype: a rdflib.URI
     Returns:
-        a tuple of the result.
+        a list of the result.
     '''
     # SPARQL Query
     sparql = SPARQLWrapper(blazegraph_url)
@@ -126,4 +161,8 @@ if __name__ == "__main__":
     For testing...
     '''
     print log_file
-    print query(gu(':spfy1'),gu(':spfy2'),gu(':Marker'))
+    #print query(gu(':spfy1'),gu(':spfy2'),gu(':Marker'))
+    print get_tyes()
+    print get_instances(gu(:'spfyId'))
+    print get_attribute_types(gu(:'spfyId'))
+    print get_attribute_values(gu(:'spfyId'), gu('so:0001076'))
