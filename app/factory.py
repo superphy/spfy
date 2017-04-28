4aa0c3cd9cbbe7b0009c84fcb1718b113097256a
Kevin Le <kevin.kent.le@gmail.com>
2017-04-28 11:18:03 -0600

    FIX: disabled strict_slashes req for routes

--- app/factory.py
+++ app/factory.py
@@ -14,6 +14,11 @@ def create_app():
     app.config.from_object(config)
     app.config.from_envvar('LOCAL_SETTINGS', silent=True)
 
+    # disable strict slashes
+    # this makes routes defined without trailing / applicable to requests with or w/o the slashes
+    # ex. @bp.route('/users') can be accessed via /users or /users/
+    app.url_map.strict_slashes = False
+
     # add extensions to app
     Bootstrap(app)
     recaptcha = ReCaptcha()
