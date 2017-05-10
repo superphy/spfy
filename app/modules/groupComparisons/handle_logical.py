fae02f4808a46398b37b495a9be779e0c9094d86
Kevin Le <kevin.kent.le@gmail.com>
2017-05-10 10:41:10 -0600

    START: first draft of code to handle logical oeprators

--- app/modules/groupComparisons/handle_logical.py
+++ app/modules/groupComparisons/handle_logical.py
@@ -16,10 +16,12 @@ def handle_logical(group):
     # create a blank universe which is our end result
     universe = set()
     for index, d in enumerate(group):
+        # d is of form: {"negated":false,"relation":"http://purl.obolibrary.org/obo/GENEPIO_0001076","attribute":"O136","logical":null}
+
         # create a temporary set before we merge with universe
         current_set = set()
-        # d is of form: {"negated":false,"relation":"http://purl.obolibrary.org/obo/GENEPIO_0001076","attribute":"O136","logical":null}
-        ### get the initial set
+
+        # get the initial set
         if d['negated']:
             # get everything not d
             log.info('Handling negation for pair: ' + d['relation'] + d['attribute'])
@@ -28,8 +30,31 @@ def handle_logical(group):
         else:
             # get spfyids in regular fashion
             current_set = resolve_spfyids(d['relation'], d['attribute'])
-        log.info(len(current_set))
+        log.info('Length of current set: ' + str(len(current_set)))
+        log.info('Length of current universe: ' + str(len(universe)))
+
+        # handle any logical operators, if applicable
+        if index > 0 and 'logical' in group[index-1].keys():
+            # note: a logical operator should be REQUIRED if relation-attribute pairs > 1
+            operator = group[index-1]['logical']
+            if operator == 'AND':
+                universe.intersection(current_set)
+            elif operator == 'OR':
+                universe.union(current_set)
+            log.info('Length of universe after logical: ' + str(len(universe)))
+        else:
+            # is index <= 0 (then index=0) and it is the current universe
+            universe = current_set
+            log.info('Length of initial universe: ' + str(len(universe)))
+    return universe
 
 if __name__ == "__main__":
-    d = [{"negated":True,"relation":"http://purl.obolibrary.org/obo/GENEPIO_0001076","attribute":"O157"}]
-    handle_logical(d)
+    d1 = {"negated":True,"relation":"http://purl.obolibrary.org/obo/GENEPIO_0001076","attribute":"O157",
+    "logical":"OR"}
+    u_d1 = handle_logical([d1])
+    log.info('*Length of u_d1: ' + str(len(u_d1)))
+    d2 = {"negated":True,"relation":"http://purl.obolibrary.org/obo/GENEPIO_0001076","attribute":"O136"}
+    u_d2 = handle_logical([d2])
+    log.info('*Length of u_d2: ' + str(len(u_d2)))
+    u_d1_or_d2 = handle_logical([d1,d2])
+    log.info('*Length of u_d1_or_d2: ' + str(len(u_d1_or_d2)))
