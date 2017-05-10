31a33c3dca250274681ff82b98f134eb07c9f019
Kevin Le <kevin.kent.le@gmail.com>
2017-05-10 12:35:13 -0600

    DEBUG: added loggging to fishers

--- app/modules/groupComparisons/groupscomparisons.py
+++ app/modules/groupComparisons/groupscomparisons.py
@@ -42,6 +42,7 @@ def groupcomparisons(groups, target):
 
     # enum through the groups and parse negations/logical operators to build a set of spfyids
     for index, group in enumerate(groups):
+        log.info(index)
         sets_spfyids.append(handle_logical(group))
         # define a blank dictionary for that spfyid
         dicts_targets.append({})
