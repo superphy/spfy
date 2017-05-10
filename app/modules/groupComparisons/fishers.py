95b9dbecd37debe5d5e807dfc362c72e71d1b880
Kevin Le <kevin.kent.le@gmail.com>
2017-05-10 14:50:16 -0600

    STOP: works now for OR, moving to test other operators

--- app/modules/groupComparisons/fishers.py
+++ app/modules/groupComparisons/fishers.py
@@ -20,23 +20,23 @@ def fishers(queryAttributeUriA, queryAttributeUriB, targetUri, results):
     dictA = results[0]['d']
     ## dictionary with the results from B
     dictB = results[1]['d']
-    log.info(str(nA))
-    log.info(str(nB))
-    log.info(str(len(dictA)))
-    log.info(str(len(dictB)))
+    log.debug(str(nA))
+    log.debug(str(nB))
+    log.debug(str(len(dictA)))
+    log.debug(str(len(dictB)))
 
     # join all possible targets as req for Fisher's
     # we could instead query the db for `DISTINCT ?s WHERE ?s a targetUri`, but I'm not interested in targets that neither queryA nor queryB has.
     all_targets = set(dictA.keys())
     all_targets.update(dictB.keys())
-    log.info('Length of All Targets: ' + str(len(all_targets)))
+    log.debug('Length of All Targets: ' + str(len(all_targets)))
 
     # create a pandas dataframe for storing aggregate results from fisher's
     df = pd.DataFrame(columns=['target','queryA','queryB','presentQueryA','absentQueryA','presentQueryB','absentQueryB','pvalue','oddsratio'])
 
     # iterate through targets and perform fisher's
     for index, target in enumerate(all_targets):
-        log.info('Target: ' + target)
+        log.debug('Target: ' + target)
         # tags for dataframe
         queryA = queryAttributeUriA
         queryB = queryAttributeUriB
@@ -46,16 +46,16 @@ def fishers(queryAttributeUriA, queryAttributeUriB, targetUri, results):
         else:
             presentQueryA = 0
         absentQueryA = nA - presentQueryA
-        log.info('presentQueryA: ' + str(presentQueryA))
-        log.info('absentQueryA: ' + str(absentQueryA))
+        log.debug('presentQueryA: ' + str(presentQueryA))
+        log.debug('absentQueryA: ' + str(absentQueryA))
         # check if target is found in queryB
         if target in dictB.keys():
             presentQueryB = len(dictB[target])
         else:
             presentQueryB = 0
         absentQueryB = nB - presentQueryB
-        log.info('presentQueryB: ' + str(presentQueryB))
-        log.info('absentQueryB: ' + str(absentQueryB))
+        log.debug('presentQueryB: ' + str(presentQueryB))
+        log.debug('absentQueryB: ' + str(absentQueryB))
         if absentQueryB < 0 or absentQueryA < 0:
             log.error('A:')
             log.error(dictA[target])
