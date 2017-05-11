9834f1ef9f96431a4097609d70cdf026a8a794a7
Kevin Le <kevin.kent.le@gmail.com>
2017-05-11 12:22:08 -0600

    UPDATE: changed default behaviour to return just the rq jobid

--- app/modules/gc.py
+++ app/modules/gc.py
@@ -16,3 +16,4 @@ multiples_q = Queue('multiples', connection=redis_conn, default_timeout=600)
 def blob_gc_enqueue(query, target):
     job_gc = multiples_q.enqueue(groupcomparisons, query, target)
     log.info('JOB ID IS: ' + job_gc.get_id())
+    return job_gc.get_id()
--- app/routes/views.py
+++ app/routes/views.py
@@ -30,9 +30,10 @@ def handle_group_comparison_submission():
     # queryAttributeTypeUriB = query[1][0]['relation']
     # targetUri = target
     # f = fishers(queryAttributeUriA, queryAttributeUriB, targetUri, queryAttributeTypeUriA, queryAttributeTypeUriB)
-    blob_gc_enqueue(query, target)
-    f = groupcomparisons(query, target)
-    return f
+    jobid = blob_gc_enqueue(query, target)
+    #f = groupcomparisons(query, target)
+    #return f
+    return jobid
 
 @bp.route('/api/v0/get_attribute_values/type/<path:attributetype>')
 def call_get_attribute_values(attributetype):
