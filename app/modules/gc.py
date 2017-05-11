0e0f95015140715eef508cb6dbf47b99830a4991
Kevin Le <kevin.kent.le@gmail.com>
2017-05-11 11:45:33 -0600

    ADD: pip to log

--- app/modules/gc.py
+++ app/modules/gc.py
@@ -1,7 +1,13 @@
-import redis
+import logging
 import config
+import redis
 from rq import Queue
 from modules.groupComparisons.groupcomparisons import groupcomparisons
+from modules.loggingFunctions import initialize_logging
+
+# logging
+log_file = initialize_logging()
+log = logging.getLogger(__name__)
 
 redis_url = config.REDIS_URL
 redis_conn = redis.from_url(redis_url)
@@ -9,4 +15,4 @@ multiples_q = Queue('multiples', connection=redis_conn, default_timeout=600)
 
 def blob_gc_enqueue(query, target):
     job_gc = multiples_q.enqueue(groupcomparisons, query, target)
-    print 'JOB ID IS: ' + job_gc.get_id()
+    log.info('JOB ID IS: ' + job_gc.get_id())
