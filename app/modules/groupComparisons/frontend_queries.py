a5581597c4ee0822a909e662370edd9086f4ac13
Kevin Le <kevin.kent.le@gmail.com>
2017-05-12 15:23:39 -0600

    DEBUG: added is_group call to get_attribute_values api

--- app/modules/groupComparisons/frontend_queries.py
+++ app/modules/groupComparisons/frontend_queries.py
@@ -56,6 +56,7 @@ def get_attribute_values(attributeTypeUri):
     Given an attribute type(ex. ge:0001076, aka. O-Type).
     Returns a list of all distinct attribute values.
     '''
+    is_group(attributeTypeUri)
     # SPARQL Query
     query = """
     SELECT DISTINCT ?attribute WHERE {{
