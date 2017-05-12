9f590388d7b0375bfbf90a997e201ec501866351
Kevin Le <kevin.kent.le@gmail.com>
2017-05-12 15:52:08 -0600

    FIX: changed to multilines in blacklist

--- app/routes/blacklist.py
+++ app/routes/blacklist.py
@@ -1,8 +1,35 @@
-blacklist = set(["http://purl.org/dc/elements/1.1/identifier", "http://biohackathon.org/resource/faldo#Begin", "http://biohackathon.org/resource/faldo#ExactPosition", "https://www.github.com/superphy#isFoundIn", "http://biohackathon.org/resource/faldo#Reference", "http://biohackathon.org/resource/faldo#End",   "http://www.w3.org/1999/02/22-rdf-syntax-ns#List", "http://biohackathon.org/resource/faldo#Region", "http://www.w3.org/2000/01/rdf-schema#domain", "https://www.github.com/superphy#hasPart", "http://biohackathon.org/resource/faldo#ReverseStrandPosition", "http://purl.org/dc/elements/1.1/description", "http://www.w3.org/2000/01/rdf-schema#Class", "http://www.w3.org/2000/01/rdf-schema#subPropertyOf", "http://www.w3.org/2000/01/rdf-schema#Datatype", "http://www.w3.org/2002/07/owl#ObjectProperty", "http://www.w3.org/1999/02/22-rdf-syntax-ns#type", "http://www.biointerchange.org/gfvo#Description",  "http://www.w3.org/2002/07/owl#TransitiveProperty", "http://www.w3.org/2000/01/rdf-schema#subClassOf", 
-  "http://purl.obolibrary.org/obo/SO_0001462",
-  "http://www.biointerchange.org/gfvo#Contig",
-  "http://www.w3.org/2000/01/rdf-schema#Resource",
-  "http://www.biointerchange.org/gfvo#Genome",
-  "http://www.w3.org/2000/01/rdf-schema#range",
-  "http://purl.org/dc/elements/1.1/date",
-  "http://www.w3.org/1999/02/22-rdf-syntax-ns#Property" ])
+# a set of blacklisted terms that reactapp user can't select from
+blacklist = set([
+    "http://purl.org/dc/elements/1.1/identifier",
+    "http://biohackathon.org/resource/faldo#Begin",
+    "http://biohackathon.org/resource/faldo#ExactPosition",
+    "https://www.github.com/superphy#isFoundIn",
+    "http://biohackathon.org/resource/faldo#Reference",
+    "http://biohackathon.org/resource/faldo#End",
+    "http://www.w3.org/1999/02/22-rdf-syntax-ns#List",
+    "http://biohackathon.org/resource/faldo#Region",
+    "http://www.w3.org/2000/01/rdf-schema#domain",
+    "https://www.github.com/superphy#hasPart",
+    "http://biohackathon.org/resource/faldo#ReverseStrandPosition",
+    "http://purl.org/dc/elements/1.1/description",
+    "http://www.w3.org/2000/01/rdf-schema#Class",
+    "http://www.w3.org/2000/01/rdf-schema#subPropertyOf",
+    "http://www.w3.org/2000/01/rdf-schema#Datatype",
+    "http://www.w3.org/2002/07/owl#ObjectProperty",
+    "http://www.w3.org/1999/02/22-rdf-syntax-ns#type",
+    "http://www.biointerchange.org/gfvo#Description",
+    "http://www.w3.org/2002/07/owl#TransitiveProperty",
+    "http://www.w3.org/2000/01/rdf-schema#subClassOf",
+    "http://purl.obolibrary.org/obo/SO_0001462",
+    "http://www.biointerchange.org/gfvo#Contig",
+    "http://www.w3.org/2000/01/rdf-schema#Resource",
+    "http://www.biointerchange.org/gfvo#Genome",
+    "http://www.w3.org/2000/01/rdf-schema#range",
+    "http://purl.org/dc/elements/1.1/date",
+    "http://www.w3.org/1999/02/22-rdf-syntax-ns#Property"
+])
+
+# a mapping of the readable form of a uri to their actual uri in blazegraph
+readable = {
+    
+}
