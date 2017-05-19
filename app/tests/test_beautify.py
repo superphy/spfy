2a7265831bebf961b8b55170345061361ab91404
Kevin Le <kevin.kent.le@gmail.com>
2017-05-19 16:58:01 -0700

    FIX: copying

--- app/tests/test_beautify.py
+++ app/tests/test_beautify.py
@@ -7,5 +7,6 @@ from tests.constants import ARGS_DICT, BEAUTIFY_VF_SEROTYPE
 def test_beautify():
     vf_serotype_gene_dict = os.path.join('tests/refs', 'GCA_000005845.2_ASM584v2_genomic.fna_ectyper-vf_serotype.p')
     gene_dict = pickle.load(open(vf_serotype_gene_dict, 'rb'))
-    single_dict = ARGS_DICT + {'i': vf_serotype_gene_dict}
+    single_dict = dict(ARGS_DICT)
+    single_dict.update({'i': vf_serotype_gene_dict})
     assert beautify(single_dict, gene_dict) == BEAUTIFY_VF_SEROTYPE
