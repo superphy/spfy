887a2057cf7dadca6e33b368ad1a1b637c329652
Kevin Le <kevin.kent.le@gmail.com>
2017-05-20 15:40:13 -0700

    DEBUG: del works, checking json gen

--- app/modules/beautify/beautify.py
+++ app/modules/beautify/beautify.py
@@ -198,6 +198,8 @@ def json_return(args_dict, gene_dict):
                             instance_dict['hitcutoff'] = args_dict['pi']
                         json_r.append(instance_dict)
 
+    log.info('First parse into json_r: ' + str(json_r))
+
     json_r = check_alleles(json_r)
     #return json_r
     # check if we tried to beautify a failed analysis
