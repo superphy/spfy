21e3564f901c58930f6967e8e3030f91b8af4d6c
Kevin Le <kevin.kent.le@gmail.com>
2017-05-19 17:03:01 -0700

    FIX: hard to compare

--- app/tests/test_beautify.py
+++ app/tests/test_beautify.py
@@ -8,4 +8,4 @@ def test_beautify():
     vf_serotype_gene_dict = os.path.join('tests/refs', 'GCA_000005845.2_ASM584v2_genomic.fna_ectyper-vf_serotype.p')
     single_dict = dict(ARGS_DICT)
     single_dict.update({'i': vf_serotype_gene_dict})
-    assert beautify(single_dict, vf_serotype_gene_dict) == BEAUTIFY_VF_SEROTYPE
+    assert len(beautify(single_dict, vf_serotype_gene_dict)) == len(BEAUTIFY_VF_SEROTYPE)
