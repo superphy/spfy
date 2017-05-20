5d5e26ea2127b656b1b75e6e6f549f971297b87a
Kevin Le <kevin.kent.le@gmail.com>
2017-05-20 16:39:27 -0700

    ADD: test for this

--- app/tests/test_beautify.py
+++ app/tests/test_beautify.py
@@ -1,7 +1,7 @@
 import os
 import pytest
 import cPickle as pickle
-from modules.beautify.beautify import beautify
+from modules.beautify.beautify import beautify, json_return
 from tests.constants import ARGS_DICT, BEAUTIFY_VF_SEROTYPE
 
 def test_beautify():
@@ -16,6 +16,12 @@ def test_beautify():
     # we only differentiate what is returned to the user, because we want all analysis ran & added to the db
     # this mimicks user selection of serotype only
     single_dict.update({'options':{'vf': False, 'amr': False, 'serotype': True}})
+    # beautify is what is actually called by the RQ worker & returned to the user
     r = beautify(single_dict, vf_serotype_gene_dict)
     print r
+    assert len(r) == 1
+    ## test json_r separately of failed handling
+    # json_return() is a part of the beautify work
+    gene_dict = pickle.load(open(vf_serotype_gene_dict, 'rb'))
+    r = json_return(args_dict, gene_dict)
     assert len(r) == 1
