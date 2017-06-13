ad158612e5b63097aa2969abf8094217ecdef5e5
Kevin Le <kevin.kent.le@gmail.com>
2017-06-13 11:12:30 -0600

    START: first draft of new workflow to poll grouped jobs server-side

--- app/routes/views.py
+++ app/routes/views.py
@@ -22,8 +22,6 @@ def job_status(job_id):
     else:
         return "Still pending", 202
 
-# the /api/v0 prefix is set to allow CORS for any postfix
-@bp.route('/api/v0/upload', methods=['POST'])
 # this is the standard route in spfy
 @bp.route('/upload', methods=['POST'])
 def upload():
