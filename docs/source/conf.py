ac529a8ebaae12d690aec64e5148da03fc7fc54a
Kevin Le <kevin.kent.le@gmail.com>
2017-06-16 12:34:43 -0600

    ADD: .rst for README

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,11 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+from recommonmark.parser import CommonMarkParser
 
+source_parsers = {
+    '.md': CommonMarkParser,
+}
 
 # -- General configuration ------------------------------------------------
 
@@ -39,8 +43,8 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+#source_suffix = '.rst'
 
 # The master toctree document.
 master_doc = 'index'
@@ -152,6 +156,3 @@ texinfo_documents = [
      author, 'Superphy', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
