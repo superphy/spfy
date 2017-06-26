16242770524e2cee265199959ef7fc362efa2e07
Kevin Le <kevin.kent.le@gmail.com>
2017-06-26 14:32:56 -0600

    FIX: naming of database module

--- app/routes/ra_module_databse.py
+++ app/routes/ra_module_databse.py
@@ -1,9 +1,8 @@
 from flask import Blueprint
-from modules.database import blob_db_enqueue
+from modules.db import blob_db_enqueue
 
 bp_ra_db = Blueprint('reactapp_module_database', __name__)
 
-# this is for the Group Comparisons (Fishers) module
 @bp_ra_db.route('/api/v0/newdatabasestatus', methods=['POST'])
 def handle_group_comparison_submission():
     jobid = blob_db_enqueue()
