48d0af87cc9d53730a1aaa579e370ab52a7dcbfc
Kevin Le <kevin.kent.le@gmail.com>
2017-07-04 09:57:08 -0600

    ADD: more comments

--- app/modules/spfy.py
+++ app/modules/spfy.py
@@ -44,6 +44,11 @@ def blob_savvy_enqueue(single_dict):
     Handles enqueueing of single file to multiple queues.
     :param f: a fasta file
     :param single_dict: single dictionary of arguments
+        ex. {'disable_amr': False, 'i': '/datastore/2017-06-30-21-53-27-595283-GCA_000023365.1_ASM2336v1_genomic.fna', 'disable_vf': False, 'disable_serotype': False, 'pi': 90, 'options': {'pi': 90, 'amr': False, 'serotype': True, 'vf': True}}}
+        Where `options` is the user-selected choices for serotyping and
+        the disable_* is used for running Ectyper; these are separate so we can
+        always run ectyper in singles/backlog_singles while still returning to
+        the user only what they selected.
     :return: dictionary with jobs ids and relevant headers
     '''
     jobs = {}
@@ -70,6 +75,8 @@ def blob_savvy_enqueue(single_dict):
         job_ectyper_beautify = ectyper_jobs['job_ectyper_beautify']
     # or if the backlog queue is enabled
     elif config.BACKLOG_ENABLED:
+        # we need to create a dict with these options enabled:
+
         # just enqueue the jobs, we don't care about returning them
         ectyper_pipeline(backlog_singles_q, backlog_multiples_q)
     #### END ECTYPER PIPELINE
