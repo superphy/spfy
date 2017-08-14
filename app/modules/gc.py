e05cfe0de6b468c5094ba88a1bf4e00c3b21430e
Kevin Le <kevin.kent.le@gmail.com>
2017-08-14 10:12:50 -0600

    CHANGE: have group comparisons run on the priority

--- app/modules/gc.py
+++ app/modules/gc.py
@@ -11,9 +11,9 @@ log = logging.getLogger(__name__)
 
 redis_url = config.REDIS_URL
 redis_conn = redis.from_url(redis_url)
-multiples_q = Queue('multiples', connection=redis_conn, default_timeout=600)
+priority_q = Queue('priority', connection=redis_conn, default_timeout=600)
 
 def blob_gc_enqueue(query, target):
-    job_gc = multiples_q.enqueue(groupcomparisons, query, target, result_ttl=-1)
+    job_gc = priority_q.enqueue(groupcomparisons, query, target, result_ttl=-1)
     log.info('JOB ID IS: ' + job_gc.get_id())
     return job_gc.get_id()
