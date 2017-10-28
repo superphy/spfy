d9a127ea89080810307b70e3c419d471e2d8fd05
Kevin Le <kevin.kent.le@gmail.com>
2017-10-28 17:20:06 +0200

    DEBUG: use the old simple file saving

--- app/routes/ra_posts.py
+++ app/routes/ra_posts.py
@@ -199,22 +199,7 @@ def measure_spent_time():
 def upload():
     recaptcha = ReCaptcha(app=current_app)
     if recaptcha.verify():
-        # new file saving
-        print 'upload(): in new file saving'
-        def custom_stream_factory(total_content_length, filename, content_type, content_length=None):
-            tmpfile = tempfile.NamedTemporaryFile('wb+', prefix='flaskapp')
-            print "start receiving file ... filename => " + str(tmpfile.name)
-            return tmpfile
-        ms = measure_spent_time()
-
-        stream,form,files = werkzeug.formparser.parse_form_data(flask.request.environ, stream_factory=custom_stream_factory)
-        total_size = 0
-
-        for fil in files.values():
-            print " ".join(["saved form name", fil.name, "submitted as", fil.filename, "to temporary file", fil.stream.name])
-            total_size += os.path.getsize(fil.stream.name)
-        mb_per_s = "%.1f" % ((total_size / (1024.0*1024.0)) / ((1.0+ms(raw=True))/1000.0))
-        print " ".join([str(x) for x in ["handling POST request, spent", ms(), "ms.", mb_per_s, "MB/s.", "Number of files:", len(files.values())]])
+        form = request.form
 
         # old file saving
         form = request.form
