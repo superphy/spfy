cf8e19ddf210a4767b34536ff0ef73fbb06ca29d
Kevin Le <kevin.kent.le@gmail.com>
2017-11-20 17:18:24 -0500

    FIX: more typos

--- app/tests/test_metadata.py
+++ app/tests/test_metadata.py
@@ -4,58 +4,9 @@ from modules.metadata.metadata import generate_metadata_graph, read
 metadata_sheet = 'tests/example_metadata.xlsx'
 
 def test_read_excel():
-    assert type(read(metadata_sheet)) is pandas.core.frame.DataFram
+    assert type(read(metadata_sheet)) is pandas.core.frame.DataFrame
 
 def test_metadata():
     df = read(metadata_sheet)
     graph = generate_metadata_graph(df)
     assert type(graph) is rdflib.graph.Graph
-
-def test_generate_uri():
-    # test generate usage:
-    e = URIRef(u'http://www.biointerchange.org/gfvo#Contig')
-    r = gu('g:Contig')
-    assert r == e
-
-    # test spfyid generation (ie. root namespace)
-    e = URIRef(u'https://www.github.com/superphy#spfy1')
-    r = gu(':spfy1')
-    assert r == e
-
-    # test cases where you call gu() with and existing URIRef and want to
-    # append something
-    # ex. gu(uriGenome, "/contigs") in datastruct_savvy.py
-    # e was previously generated
-    e = URIRef(u'https://www.github.com/superphy#1947b0815e6c1b11565e1ef5db9c884e3eead520/contigs')
-    # mock_filehash mocks the hash of a genome file
-    mock_filehash = sha1('thisvaluedoesntmatterfornow').hexdigest()
-    # generate the mock uriGenome
-    # this returns a rdflib.URIRef object
-    mock_uriGenome = gu(':' + mock_filehash)
-    # this is the test case, r is a mock uriContigs
-    r = gu(mock_uriGenome, '/contigs')
-    assert r == e
-
-    # test with a url in the form of a string
-    e = URIRef('https://www.github.com/superphy#')
-    r = gu('https://www.github.com/superphy#')
-    assert r == e
-
-def test_actual_filename():
-    # test filenames with timestamps
-    s = '/datastore/2017-08-10-14-57-59-722994-GCA_900096815.1_Ecoli_AG100_Sample2_M9_Assembly_genomic.fna'
-    e = 'GCA_900096815.1_Ecoli_AG100_Sample2_M9_Assembly_genomic.fna'
-    r = actual_filename(s)
-    assert r == e
-
-    # test filenames without timestamps
-    s = '/datastore/GCA_900096815.1_Ecoli_AG100_Sample2_M9_Assembly_genomic.fna'
-    e = 'GCA_900096815.1_Ecoli_AG100_Sample2_M9_Assembly_genomic.fna'
-    r = actual_filename(s)
-    assert r == e
-
-    # test filenames without timestamps or paths
-    s = 'GCA_900096815.1_Ecoli_AG100_Sample2_M9_Assembly_genomic.fna'
-    e = 'GCA_900096815.1_Ecoli_AG100_Sample2_M9_Assembly_genomic.fna'
-    r = actual_filename(s)
-    assert r == e
