9279bb2b16ebc1b1659487c222f79e0b479a72ff
Kevin Le <kevin.kent.le@gmail.com>
2017-11-23 13:54:41 -0500

    START: tests against the webserver api

--- app/routes/alive.py
+++ app/routes/alive.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 bp_alive = Blueprint('alive', __name__)
 
-@bp_aliveroute('/api/v0/alive')
+@bp_alive('/api/v0/alive')
 def alive():
     '''
     Used for tests to check if the webserver is working internally.
--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -5,4 +5,5 @@ API_ROOT = 'api/v0/'
 
 def test_api():
     r = requests.get("""http://localhost:{port}/{api_root}alive""".format(port=WEBSERVER_PORT,api_root=API_ROOT))
+    # There is a route defined in the Flask API that returns the string 'true' when queried.
     assert r.text == 'true'
