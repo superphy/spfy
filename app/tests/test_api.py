f8889a5f80dfda6fc5482e50981363398be13fde
Kevin Le <kevin.kent.le@gmail.com>
2017-11-23 15:49:58 -0500

    FIX: was a typo, and ADD: run internal tests via pytest

--- app/tests/test_api.py
+++ app/tests/test_api.py
@@ -4,6 +4,17 @@ WEBSERVER_PORT = '8090'
 API_ROOT = 'api/v0/'
 
 def test_api():
+    '''
+    This is an external test that checks if 'webserver' Flask api build correctly.
+    '''
     r = requests.get("""http://localhost:{port}/{api_root}alive""".format(port=WEBSERVER_PORT,api_root=API_ROOT))
     # There is a route defined in the Flask API that returns the string 'true' when queried.
     assert r.text == 'true'
+
+import subprocess
+
+WEBSERVER = 'backend_webserver_1'
+
+def test_api_internal():
+    o = subprocess.check_output('docker exec -ti {webserver} sh -c "echo a"'.format(webserver=WEBSERVER))
+    assert o == 'a'
