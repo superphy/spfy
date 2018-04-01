ef1c32c5870bc372940603fbd8903540550519bb
Kevin Le <kevin.kent.le@gmail.com>
2018-04-01 17:46:46 -0400

    FIX?: this shouldve done it

--- app/routes/ra_posts.py
+++ app/routes/ra_posts.py
@@ -228,7 +228,7 @@ def upload():
         # get a list of files submitted
         uploaded_files = request.files.getlist("file")
         print('upload(), uploaded_files: {0}'.format(uploaded_files))
-        names = [str(file) for file in uploaded_files if file]
+        names = [secure_filename(file.filename) for file in uploaded_files if file]
         names = names.sort()
         print('upload(), names: {0}'.format(names))
 
