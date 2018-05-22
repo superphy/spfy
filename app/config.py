d6ad32a68dbbeb5ddd885667f87467cab0c70719
Kevin Le <kevin.kent.le@gmail.com>
2018-05-22 19:01:46 -0400

    DEBUG: check blazegraph connection within docker

--- app/config.py
+++ app/config.py
@@ -66,7 +66,7 @@ database = {}
 #database['blazegraph_url'] = 'http://localhost:9000/blazegraph/namespace/superphy/sparql'
 database['blazegraph_url'] = os.getenv(
     'SUPERPHY_RDF_URL',
-    'http://blazegraph:8080/blazegraph/sparql'
+    'http://blazegraph:8080/bigdata/sparql'
 )
 # database['blazegraph_url'] = os.getenv(
 #     'SUPERPHY_RDF_URL',
